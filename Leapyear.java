698107d552389953cbb8abbba319de176d0164a8
Periyasamy04 <periyasamy6804@gmail.com>
2024-07-29 11:48:26 +0530

    leapyear

--- Leapyear.java
+++ Leapyear.java
@@ -1,6 +1,7 @@
 public class Leapyear {
     public static void main(String[] args) {
         int year=2023;
+        String name;
         if(year%4==0){
             System.out.println("leap year");
         }
