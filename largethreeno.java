ad5306e9a0d9da6bc2364aac1802f74f1360eb5f
Periyasamy04 <periyasamy6804@gmail.com>
2024-07-29 12:40:43 +0530

    Largest three numbers of the given numbers

---
+++ largethreeno.java
@@ -0,0 +1,17 @@
+public class largethreeno {
+    public static void main(String[] args) {
+        int[] arr={42,445547,784857,145142,78442};
+        for(int i=0;i< arr.length;i++){
+            for(int j=0;j< arr.length;j++){
+                if(arr[i]<arr[j]){
+                    int t=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=t;
+                }
+            }
+        }
+        System.out.println("maximum of the number is: "+arr[arr.length-1]);
+        System.out.println("maximum of the number is: "+arr[arr.length-2]);
+        System.out.println("maximum of the number is: "+arr[arr.length-3]);
+    }
+}
