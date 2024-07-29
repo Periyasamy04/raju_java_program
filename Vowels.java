649f7af3758fb9a750dee07f2c0e7f54e578c45e
Periyasamy04 <periyasamy6804@gmail.com>
2024-07-29 11:35:43 +0530

    Vowels

---
+++ Vowels.java
@@ -0,0 +1,13 @@
+public class Vowels {public static void main(String[] args) {
+    String[] arr ={"apple", "ball", "collection", "meeting", "raju"};
+    for (int i = 0; i < 5; i++) {
+        int count = 0;
+        for (int j = 0; j < arr[i].length(); j++) {
+            if (arr[i].charAt(j) == 'a' || arr[i].charAt(j) == 'e' || arr[i].charAt(j) == 'i' || arr[i].charAt(j) == 'o' || arr[i].charAt(j) == 'u') {
+                count++;
+            }
+        }
+        System.out.println(arr[i] + " = " + count);
+    }
+}
+}
