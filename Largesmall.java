02a8b208062579392e6f0b671d4e3824ffe03916
Periyasamy04 <periyasamy6804@gmail.com>
2024-07-30 10:30:43 +0530

    Update Largesmall.java
    Largest and Smallest number of the given number

--- Largesmall.java
+++ Largesmall.java
@@ -1,4 +1,4 @@
-public interface Largesmall {
+public class Largesmall {
     public static void main(String[] args) {
         int[] arr={42,445547,784857,145142,78442};
         for(int i=0;i< arr.length;i++){
