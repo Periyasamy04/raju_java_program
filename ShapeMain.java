b1380a1a3547c770102f93207b43b0f40539279d
Periyasamy04 <periyasamy6804@gmail.com>
2024-08-06 15:11:25 +0530

    Shape using Inheritance and Method Overriding

---
+++ ShapeMain.java
@@ -0,0 +1,8 @@
+public class ShapeMain {
+    public static void main(String[] args) {
+        Shape circle= new Circle();
+        circle.draw();
+        Shape rectangle= new Rectangle();
+        rectangle.draw();
+    }
+}
