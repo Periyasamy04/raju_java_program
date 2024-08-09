fa6b60866bf475f2c07a0f8655249d8a471a5c0e
Periyasamy04 <periyasamy6804@gmail.com>
2024-08-09 10:28:02 +0530

    Update InvalidInputException.java

--- ExceptionHandling/InvalidInputException.java
+++ ExceptionHandling/InvalidInputException.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class InvalidInputException {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number1: ");
-        System.out.println("Enter the number2: ");
         try {
+            System.out.println("Enter the number1: ");
             int Number1= sc.nextInt();
+            System.out.println("Enter the number2: ");
             int Number2= sc.nextInt();
             int result= Number1 / Number2;
             System.out.println("the result"+ result);
