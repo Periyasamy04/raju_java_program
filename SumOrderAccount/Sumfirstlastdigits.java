0378a4e510b9d292dcc675ae28120dfecdf68dc7
Periyasamy04 <periyasamy6804@gmail.com>
2024-08-22 12:37:38 +0530

    Sum of first and last digits

---
+++ SumOrderAccount/Sumfirstlastdigits.java
@@ -0,0 +1,27 @@
+package SumOrderAccount;
+
+import java.util.Scanner;
+
+public class Sumfirstlastdigits {
+    public int sumFirstAndLastDigit( int number) {
+        int firstDigit = number;
+        while (firstDigit >= 10) {
+            firstDigit = firstDigit / 10;
+        }
+        int lastDigit = number % 10;
+
+        if (number < 0) {
+            System.out.println("Invalid number");
+            return -1;
+        }
+        return firstDigit+ lastDigit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter any number that you wish: ");
+        int value= sc.nextInt();
+        Sumfirstlastdigits sumfirstlastdigits= new Sumfirstlastdigits();
+        System.out.println(sumfirstlastdigits.sumFirstAndLastDigit(value));
+    }
+}
