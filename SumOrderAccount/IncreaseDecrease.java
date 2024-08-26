47bd311f6e99d31363c2fda5a934da4093767cad
Periyasamy04 <periyasamy6804@gmail.com>
2024-08-26 15:48:26 +0530

    Update IncreaseDecrease.java

--- SumOrderAccount/IncreaseDecrease.java
+++ SumOrderAccount/IncreaseDecrease.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 
 public class IncreaseDecrease {
 
-    public void checking(int number1, int number2, int number3){
-        if(number1 < number2 && number2 < number3){
-            System.out.print("The Given input "+ number1+","+number2+","+number3+" is in Increasing Order. ");
+    public void checking(int number1, int number2, int number3) {
+        if(number1 < number2 && number2 < number3) {
+            System.out.print("The Given input "+ number1+", "+number2+", "+number3+" is in Increasing Order. ");
         }
-        else if(number1 > number2 && number2 > number3){
-            System.out.println("The Given input"+ number1+","+number2+","+number3+"is in Decreasing Order. ");
+        else if(number1 > number2 && number2 > number3) {
+            System.out.println("The Given input " + number1 + ", " + number2 + ", " + number3 + " is in Decreasing Order. ");
         }
-        else{
+        else {
             System.out.println("The Given input is not either increasing or Decreasing. ");
         }
     }
 
     public static void main(String[] args) {
-        IncreaseDecrease risingReducing= new IncreaseDecrease();
+        IncreaseDecrease risingReducing = new IncreaseDecrease();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number1: ");
         int number1 = scanner.nextInt();
