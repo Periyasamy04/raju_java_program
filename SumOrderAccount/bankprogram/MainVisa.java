8c13aca5126f64a338ae0536d0e8e82e253e3a05
Periyasamy04 <periyasamy6804@gmail.com>
2024-08-26 16:20:15 +0530

    Update MainVisa.java

--- SumOrderAccount/bankprogram/MainVisa.java
+++ SumOrderAccount/bankprogram/MainVisa.java
@@ -8,35 +8,35 @@ public class MainVisa {
         System.out.println("Enter your Bank Name: ");
         String bankName = scanner.nextLine();
         System.out.println(" Enter Your Card Number: ");
-        String inputcardnumber = scanner.nextLine();
+        String inputCardNumber = scanner.nextLine();
         System.out.println("Enter Your Pin Number: ");
-        int inputpin = scanner.nextInt();
+        int inputPin = scanner.nextInt();
         System.out.println("Enter Your Amount: ");
-        int inputamount = scanner.nextInt();
+        int inputAmount = scanner.nextInt();
 
         AxisBank axisBank = new AxisBank();
         FederalBank federalBank = new FederalBank();
         SbiBank sbiBank = new SbiBank();
 
         if (bankName.toLowerCase().equals("axisbank")) {
-            if (axisBank.authenticate(inputcardnumber, inputpin)) {
-                System.out.println("In Axis bank Your Withdrawal Amount is: " + axisBank.withdraw(inputamount));
+            if (axisBank.authenticate(inputCardNumber, inputPin)) {
+                System.out.println("In Axis bank Your Withdrawal Amount is: " + axisBank.withdraw(inputAmount));
             } else {
                 System.out.println("Your Authentication is Failed. ");
             }
         }
 
         if (bankName.toLowerCase().equals("federalbank")) {
-            if (federalBank.authenticate(inputcardnumber, inputpin)) {
-                System.out.println("In Federal bank Your Withdrawal Amount is: " + federalBank.withdraw(inputamount));
+            if (federalBank.authenticate(inputCardNumber, inputPin)) {
+                System.out.println("In Federal bank Your Withdrawal Amount is: " + federalBank.withdraw(inputAmount));
             } else {
                 System.out.println("Your Authentication is Failed. ");
             }
         }
 
         if (bankName.toLowerCase().equals("sbibank")) {
-            if (sbiBank.authenticate(inputcardnumber, inputpin)) {
-                System.out.println("In SBI bank Your Withdrawal Amount is: " + sbiBank.withdraw(inputamount));
+            if (sbiBank.authenticate(inputCardNumber, inputPin)) {
+                System.out.println("In SBI bank Your Withdrawal Amount is: " + sbiBank.withdraw(inputAmount));
             } else {
                 System.out.println("Your Authentication is Failed. ");
             }
