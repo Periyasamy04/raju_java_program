5460c57e6afe39baba0439dd8cae2fb66ecc3983
Periyasamy04 <periyasamy6804@gmail.com>
2024-08-30 13:29:46 +0530

    Number Guessing Game using Set

--- setexample/BoxSet.java
+++ setexample/BoxSet.java
@@ -7,15 +7,15 @@ import java.util.Set;
 
 public class BoxSet {
     public static void main(String[] args) {
-
-        Set<Integer> boxA = new HashSet<>(Arrays.asList(1, 2, 3));
-        Set<Integer> boxB = new HashSet<>(Arrays.asList(2, 3, 4));
-        Set<Integer> boxC = new HashSet<>(Arrays.asList(4, 5, 6));
-        Set<Integer> boxD = new HashSet<>(Arrays.asList(5, 6, 7));
+        // Define the sets for each box
+        Set<Integer> boxA = new HashSet<>(Arrays.asList(8, 9, 10, 11, 12, 13, 14, 15));
+        Set<Integer> boxB = new HashSet<>(Arrays.asList(4, 5, 6, 7, 12, 13, 14, 15));
+        Set<Integer> boxC = new HashSet<>(Arrays.asList(2, 3, 6, 7, 10, 11, 14, 15));
+        Set<Integer> boxD = new HashSet<>(Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15));
 
         Set<Integer>[] Boxes = new Set[]{boxA, boxB, boxC, boxD};
 
-        Set<Integer> guessedNumberSet = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        Set<Integer> guessedNumberSet = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
 
         Scanner scanner = new Scanner(System.in);
 
