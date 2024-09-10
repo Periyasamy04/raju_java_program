394899312ac747bfb52f1a90b91ab06d3b2be2e0
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-10 12:57:28 +0530

    check valid email using regex

--- regexTask/GmailTask.java
+++ regexTask/GmailTask.java
@@ -5,18 +5,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GmailTask {
-    void isValidEmail(String email){
+    boolean isValidEmail(String email){
         String regexpattern="[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z]{2,4}";
         Pattern pattern = Pattern.compile(regexpattern);
         Matcher matcher = pattern.matcher(email);
 
         if(matcher.matches()){
             System.out.println("Given email is valid.");
+            return true;
         }
         else{
             System.out.println("Please enter the valid email.");
+            return false;
         }
-//        return false;
     }
 
     public static void main(String[] args) {
