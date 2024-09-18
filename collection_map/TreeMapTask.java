7a0ec673fcf31862638d448d253b52eb76c7b34d
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-18 17:21:30 +0530

    Tree Map

---
+++ collection_map/TreeMapTask.java
@@ -0,0 +1,13 @@
+package collection_map;
+
+import java.util.TreeMap;
+
+public class TreeMapTask {
+    public static void main(String[] args) {
+        TreeMap<Integer,String> treemap = new TreeMap<>();
+        treemap.put(0,"Raju");
+        treemap.put(1,"Akshaya");
+        treemap.put(2,"Naveen");
+        System.out.println(treemap);
+    }
+}
