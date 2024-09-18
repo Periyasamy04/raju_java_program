bffb30729040db7b34748c46487d9da3004f96b6
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-18 14:34:47 +0530

    Update CombineTwoArrays.java

--- collection_list/CombineTwoArrays.java
+++ collection_list/CombineTwoArrays.java
@@ -6,8 +6,15 @@ import java.util.List;
 
 public class CombineTwoArrays {
     public static void main(String[] args) {
-        List<String> list1=new ArrayList<>(Arrays.asList("Raju","sadhana","Shruti","Naveen","Akshaya"));
-        List<String> list2=new ArrayList<>(Arrays.asList("Karthick","Vicky","Hari","Lalit"));
+        List<String> list1=new ArrayList<>();
+        list1.add("Raju");
+        list1.add("Sadhana");
+        list1.add("Akshaya");
+        
+        List<String> list2=new ArrayList<>();
+        list2.add("Karthick");
+        list2.add("Lalit");
+        list2.add("Hari");
 
         list1.addAll(list2);
         System.out.println("Combined Values : "+list1);
