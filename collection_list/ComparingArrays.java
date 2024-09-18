c7b724407b8df00ce7f75ad21ba854269dee59cc
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-18 14:42:41 +0530

    Update ComparingArrays.java

--- collection_list/ComparingArrays.java
+++ collection_list/ComparingArrays.java
@@ -7,9 +7,16 @@ import java.util.List;
 
 public class ComparingArrays {
     public static void main(String[] args) {
-        List<Integer> list1=new ArrayList<>(Arrays.asList(12, 31, 14, 76));
-        List<Integer> list2=new ArrayList<>(Arrays.asList(91, 18, 14, 65));
+        List<Integer> list1=new ArrayList<>();
+        list1.add(12);
+        list1.add(78);
+        list1.add(32);
 
+        List<Integer> list2=new ArrayList<>();
+        list2.add(12);
+        list2.add(78);
+        list2.add(32);
+        
         if(list1.equals(list2)){
             System.out.println("Equal.");
         }else{
