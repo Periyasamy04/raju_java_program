cd8865ec9e9e3662662c351877eb764e8c4465dc
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-18 17:05:26 +0530

    Associate specified values for a specified key

---
+++ collection_map/HashMapTask.java
@@ -0,0 +1,15 @@
+package collection_map;
+
+
+import java.util.HashMap;
+
+public class HashMapTask {
+    public static void main(String[] args) {
+        HashMap <Integer,String> map = new HashMap<>();
+        map.put(0,"Raju");
+        map.put(1,"Akshaya");
+        map.put(2,"Naveen");
+
+        System.out.println(map);
+    }
+}
