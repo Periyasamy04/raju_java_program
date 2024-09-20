c52e557c803e4e2d6b81d6f997da0b51fb0954bb
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-20 12:02:21 +0530

    Create a table in SQL using JDBC

---
+++ JDBC/CreateTableJDBC.java
@@ -0,0 +1,27 @@
+package JDBC;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTableJDBC {
+
+    public static void main(String[] args) {
+        String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=interndb_periyasamy;encrypt=true;trustServerCertificate=true";
+        String user = "periyasamy";
+        String password = "Raju@6804";
+        String retrieve= "SELECT * FROM Books";
+        String createtableSQL= "CREATE TABLE books("+"Book_ID INT PRIMARY KEY," + "Book_Name VARCHAR(50)," + "Book_Author VARCHAR(50))";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement= connection.createStatement();
+            statement.executeUpdate(createtableSQL);
+            System.out.println("Table 'Books' Created Successfully. ");
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
