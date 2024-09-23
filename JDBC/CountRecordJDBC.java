00552861bb89a6aca2ae81fa5d5ecc955ba29b64
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-23 12:20:13 +0530

    Count Record using JDBC

--- JDBC/CountRecordJDBC.java
+++ JDBC/CountRecordJDBC.java
@@ -7,10 +7,10 @@ public class CountRecordJDBC {
         String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=interndb_periyasamy;encrypt=true;trustServerCertificate=true";
         String user = "periyasamy";
         String password = "Raju@6804";
+        String CountQuary= "SELECT COUNT(*) AS count FROM Books";
 
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
-            String CountQuary= "SELECT COUNT(*) AS count FROM Books";
             Statement statement= connection.createStatement();
             ResultSet resultSet= statement.executeQuery(CountQuary);
             resultSet.next();
