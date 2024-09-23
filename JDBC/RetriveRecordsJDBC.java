34ae724c708d358a702958f68d0461c5704f56ac
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-23 13:09:49 +0530

    Filtering Record using JDBC

--- JDBC/RetriveRecordsJDBC.java
+++ JDBC/RetriveRecordsJDBC.java
@@ -7,12 +7,12 @@ public class RetriveRecordsJDBC {
         String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=interndb_periyasamy;encrypt=true;trustServerCertificate=true";
         String user = "periyasamy";
         String password = "Raju@6804";
-        String retrieveSQL= "SELECT * FROM Books";
+        String retrieveQuery= "SELECT * FROM Books";
 
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement statement= connection.createStatement();
-            ResultSet resultSet= statement.executeQuery(retrieveSQL);
+            ResultSet resultSet= statement.executeQuery(retrieveQuery);
             while(resultSet.next()) {
                 int BookID= resultSet.getInt("Book_id");
                 String BookName= resultSet.getString("Book_Name");
