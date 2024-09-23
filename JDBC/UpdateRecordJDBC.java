0fc560ec2b1bac37e2872d22b33a29c9c71e8207
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-23 11:04:48 +0530

    Update Record using JDBC

---
+++ JDBC/UpdateRecordJDBC.java
@@ -0,0 +1,35 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class UpdateRecordJDBC {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the Book Name You Want to Update: ");
+        String NewBookName= scanner.nextLine();
+        System.out.print("Enter the Book ID: ");
+        int BookID= scanner.nextInt();
+        String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=interndb_periyasamy;encrypt=true;trustServerCertificate=true";
+        String user = "periyasamy";
+        String password = "Raju@6804";
+
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            String UpdateQuary= "UPDATE Books SET Book_Name=? WHERE Book_ID=?";
+            PreparedStatement statement= connection.prepareStatement(UpdateQuary);
+            statement.setString(1,NewBookName);
+            statement.setInt(2,BookID);
+            statement.executeUpdate();
+            System.out.println("The Record is updated Successfully. ");
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
