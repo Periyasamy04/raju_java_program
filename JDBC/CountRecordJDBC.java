cbcdfed3e483053c71cfe780b1180aa2015bec47
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-23 13:10:06 +0530

    Filtering Record using JDBC

--- JDBC/CountRecordJDBC.java
+++ JDBC/CountRecordJDBC.java
@@ -7,12 +7,12 @@ public class CountRecordJDBC {
         String url = "jdbc:sqlserver://192.168.0.49\\HEALNET;databaseName=interndb_periyasamy;encrypt=true;trustServerCertificate=true";
         String user = "periyasamy";
         String password = "Raju@6804";
-        String CountQuary= "SELECT COUNT(*) AS count FROM Books";
+        String CountQuery= "SELECT COUNT(*) AS count FROM Books";
 
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement statement= connection.createStatement();
-            ResultSet resultSet= statement.executeQuery(CountQuary);
+            ResultSet resultSet= statement.executeQuery(CountQuery);
             resultSet.next();
             int Output= resultSet.getInt("count");
             System.out.println("The Total number of Records is: " + Output);
