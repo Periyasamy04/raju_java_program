cef3b5df6db7b63a10f66c21679ed63e734a1b87
Periyasamy04 <periyasamy6804@gmail.com>
2024-09-30 16:55:27 +0530

    Array Generics Method

---
+++ GenericsTask/ArrayGenerics.java
@@ -0,0 +1,25 @@
+package GenericsTask;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayGenerics<P> {
+
+    public <P> void printArray(P[] arrayinput){
+        for(P elements:arrayinput)
+            System.out.println(elements + " ");
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        ArrayGenerics arrayGenerics=new ArrayGenerics();
+        System.out.print("Enter the Size of the Array: ");
+        int size=scanner.nextInt();
+
+        scanner.nextLine();
+        Integer[] array= new Integer[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        arrayGenerics.printArray(array);
+    }
+}
