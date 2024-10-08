aabbb3564d75010ef5b92e64316f89b316b07805
Periyasamy04 <periyasamy6804@gmail.com>
2024-10-08 14:29:32 +0530

    Create Entity

--- JDBC_2_create_Entity/CreateEntity.java
+++ JDBC_2_create_Entity/CreateEntity.java
@@ -1,6 +1,6 @@
 package JDBC_2_create_Entity;
 public class CreateEntity {
-    public int Employee_ID;
+    public String Employee_ID;
     public String Frist_Name;
     public String Last_Name;
     public String Middle_Name;
@@ -8,13 +8,13 @@ public class CreateEntity {
     public String Gender;
     public String Email_Address;
     public String Phone_Number;
-    public String Address;
+    public Address address;
 
-    public CreateEntity(int Employee_ID, String Frist_Name,
+    public CreateEntity(String Employee_ID, String Frist_Name,
                         String Last_Name, String Middle_Name,
                         String Date_Of_Birth, String Gender,
                         String Email_Address, String Phone_Number,
-                        String Address){
+                        Address address){
         this.Employee_ID = Employee_ID;
         this.Frist_Name = Frist_Name;
         this.Last_Name = Last_Name;
@@ -23,7 +23,7 @@ public class CreateEntity {
         this.Gender = Gender;
         this.Email_Address = Email_Address;
         this.Phone_Number = Phone_Number;
-        this.Address = Address;
+        this.address = address;
     }
 
     @Override
@@ -31,15 +31,16 @@ public class CreateEntity {
         return Employee_ID + "\n" + Frist_Name + "\n" +
                 Last_Name + "\n" + Middle_Name + "\n" +
                 Date_Of_Birth + "\n" + Gender + "\n" +
-                Email_Address + "\n" + Phone_Number + "\n" + Address;
+                Email_Address + "\n" + Phone_Number + "\n" + address;
     }
 
     public static void main(String[] args) {
-        CreateEntity createEntity=new CreateEntity(123,"Raju",
+        Address address1 =new Address("NO:5/9 Banu Nagar","2nd Avenue Ambattur","Chennai","TamilNadu","600099");
+        CreateEntity createEntity=new CreateEntity("CI2404","Raju",
                 "S","Bhai",
                 "06-08-2004","male",
                 "periyasamy6804@gmail.com",
-                "9677011150","Ambattur");
+                "9677011150",address1);
         System.out.println(createEntity);
     }
 }
