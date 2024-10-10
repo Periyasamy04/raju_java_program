b27c3581a333bc4b0fab1e1267587804e8c01f67
Periyasamy04 <periyasamy6804@gmail.com>
2024-10-10 17:44:12 +0530

    Employee JDBC(Eligibility)

---
+++ employeedetails/EmploymentEligibilityVerification.java
@@ -0,0 +1,20 @@
+package employeedetails;
+
+public class EmploymentEligibilityVerification {
+    public String cgpaPercentage;
+    public String degreeRegisteredNumber;
+    public String tcNumber;
+
+    public EmploymentEligibilityVerification(String cgpaPercentage, String degreeRegisteredNumber, String tcNumber) {
+        this.cgpaPercentage = cgpaPercentage;
+        this.degreeRegisteredNumber = degreeRegisteredNumber;
+        this.tcNumber = tcNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "\nCGPA Percentage: " + cgpaPercentage + "\n"
+                + "Degree Number: " + degreeRegisteredNumber + "\n"
+                + "TC Number: " + tcNumber;
+    }
+}
